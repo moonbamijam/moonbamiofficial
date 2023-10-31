c86d6c6bc8ec17029253c78563af4881cdfc342e
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2023-11-01 00:56:51 +0800

    feat: add new custom color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,8 @@ const config: Config = {
       colors: {
         'rich-black': '#03001C',
         'deep-koamaru': '#301E67',
-        'electric-pink': '#FB2576'
+        'electric-pink': '#FB2576',
+        'opaque-rb': 'rgba(3, 0, 28, 0.8)'
       },
       fontFamily: {
         poppins: ['Poppins', 'sans-serif']
