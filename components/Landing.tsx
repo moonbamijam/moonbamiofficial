e52aed4b2a91f009f09af99ae5203d4dd44f4fc7
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2023-11-01 16:49:51 +0800

    feat: add utility class

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -5,8 +5,8 @@ import Galaxy_Background from '@public/assets/images/vector-galaxy-background.jp
 
 const Landing = () => {
   return (
-    <section id="profile" className="profile-section h-[55rem] flex flex-wrap justify-center items-center gap-x-[25rem] py-0 px-[100px]">
-      <Image className="absolute right-0 z-[-1] w-full object-cover opacity-30" src={ Galaxy_Background } alt="Gradient galaxy background"/>
+    <section id="profile" className="landing-section container h-[55rem] flex flex-wrap justify-center items-center gap-x-[25rem]">
+      <Image className="absolute z-[-1] w-full h-[55rem] object-cover opacity-30" src={ Galaxy_Background } alt="Gradient galaxy background"/>
       <div className="description text-white">
         <h1 className="text-8xl font-bold tracking-[-0.96px] mb-8">Hey there!</h1>
         <p className="w-[37.5rem] text-xl font-normal leading-7 ">My name is <strong className="landing-strong">Moonbami</strong>, and I am currently pursuing a career for being a <strong className="landing-strong">full-stack web developer</strong>. I am still a novice, but I am making <strong className="landing-strong">progress and learning</strong> on a daily basis.</p>
