49e4d27f2f03074f706fc70f4ba49a5ea4ffd60c
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2023-11-08 23:56:41 +0800

    refactor: remove bg

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -1,12 +1,10 @@
 
 import Image from 'next/image';
 import Moonbami from '@public/assets/images/moonbami.jpeg';
-import Galaxy_Background from '@public/assets/images/vector-galaxy-background.jpg';
 
 const Landing = () => {
   return (
     <section id="profile" className="landing-section container h-[55rem] flex flex-wrap justify-center items-center gap-x-[25rem]">
-      <Image className="absolute z-[-1] w-full h-[55rem] object-cover opacity-30" src={ Galaxy_Background } alt="Gradient galaxy background"/>
       <div className="description text-white">
         <h1 className="text-8xl font-bold tracking-[-0.96px] mb-8">Hey there!</h1>
         <p className="w-[37.5rem] text-xl font-normal leading-7 ">My name is <strong className="landing-strong">Moonbami</strong>, and I am currently pursuing a career for being a <strong className="landing-strong">full-stack web developer</strong>. I am still a novice, but I am making <strong className="landing-strong">progress and learning</strong> on a daily basis.</p>
