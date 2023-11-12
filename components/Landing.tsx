a3a07a03b5334fdf0419ede05e1c56a9b6e172d6
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2023-11-13 00:00:34 +0800

    feat: re align landing section

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -4,7 +4,7 @@ import Moonbami from '@public/assets/images/moonbami.jpg';
 
 const Landing = () => {
   return (
-    <section id="profile" className="landing-section container h-[55rem] flex flex-wrap justify-center items-center gap-x-[25rem]">
+    <section id="profile" className="landing-section container h-[55rem] flex flex-wrap justify-evenly items-center gap-x-[25rem]">
       <div className="description text-white">
         <h1 className="text-8xl font-bold tracking-[-0.96px] mb-8">Hey there!</h1>
         <p className="w-[37.5rem] text-xl font-normal leading-7 ">My name is <strong className="landing-strong">Moonbami</strong>, and I am currently pursuing a career for being a <strong className="landing-strong">full-stack web developer</strong>. I am still a novice, but I am making <strong className="landing-strong">progress and learning</strong> on a daily basis.</p>
