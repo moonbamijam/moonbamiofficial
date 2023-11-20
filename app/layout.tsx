5ecd19fc91373cb8fc3e267a3b4785aaa696d045
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2023-11-20 23:09:28 +0800

    feat: add new title

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from 'next';
 import '@styles/globals.css';
 
 export const metadata: Metadata = {
-  title: 'Moonbami Official',
+  title: 'Moonbami',
   description: 'An aspiring full-stack web developer',
 }
 
