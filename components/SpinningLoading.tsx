8e676bd830fcee26ee7af73add60c3bc23c33109
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-01-22 21:13:43 +0800

    feat: add loading component

---
+++ components/SpinningLoading.tsx
@@ -0,0 +1,11 @@
+import { ImSpinner2 } from "react-icons/im";
+
+const SpinningLoading = () => {
+  return (
+    <div className="animate-spin">
+      <ImSpinner2 />
+    </div>
+  )
+}
+
+export default SpinningLoading
