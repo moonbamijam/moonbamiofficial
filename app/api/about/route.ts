ca39d886a0aac8c6aae4e6fc4b04f2dbab7edfd4
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-01-22 21:16:15 +0800

    feat: add api

---
+++ app/api/about/route.ts
@@ -0,0 +1,37 @@
+import connectMongoDB from "@utilities/mongodb";
+import About from "@models/about";
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request, response: Response) {
+  const { 
+    title, 
+    desc, 
+    name,
+    nickname,
+    age,
+    birthday,
+    sex,
+    nationality,
+    status,
+    languages
+  } = await request.json();
+  await connectMongoDB();
+  await About.create({ 
+    title, 
+    desc,
+    name,
+    nickname,
+    age,
+    birthday,
+    sex,
+    nationality,
+    status,
+    languages
+  });
+};
+
+export async function GET() {
+  await connectMongoDB();
+  const about = await About.find();
+  return NextResponse.json({ about })
+};
