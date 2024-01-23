53748ab18a83bf45124307eeb28a7fb729cc0f11
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-01-23 10:33:07 +0800

    feat: add new not found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,10 +1,18 @@
+import Link from "next/link"
 import { FaSadCry } from "react-icons/fa"
 
 const NotFound = () => {
   return (
-    <div className="">
-      <p>page not found</p>
-      <FaSadCry />
+    <div id="" className=" container relative">
+      <section className="content pb-[300px] mt-[200px] lg:mt-[250px] flex flex-col gap-10 items-center justify-center">
+        <h1 className="switch-text-color font-bold text-7xl text-center w-[700px]">Page not found! 
+          <h2 className="switch-text-color text-4xl capitalize">go back <Link href="/" className="text-blue-400 capitalize underline">home</Link>
+          </h2>
+        </h1>
+        <div className="rounded-full bg-black p-1 dark:bg-transparent text-3xl w-[200px]">
+          <FaSadCry />
+        </div>
+      </section>
     </div>
   )
 }
