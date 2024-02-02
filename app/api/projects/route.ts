72bead9f2fa3fe040790169d4aec03d075cdfda5
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-02-02 23:43:27 +0800

    fix: projects route not a module

--- app/api/projects/route.ts
+++ app/api/projects/route.ts
@@ -1,24 +1,24 @@
-// import connectMongoDB from "@utilities/mongodb";
-// import Project from "@models/project";
-// import { NextResponse } from "next/server";
+import connectMongoDB from "@utilities/mongodb";
+import Project from "@models/project";
+import { NextResponse } from "next/server";
 
-// export async function POST(request: Request, response: Response) {
-//   const { 
-//     title, 
-//     desc, 
-//     href
-//   } = await request.json();
-//   await connectMongoDB();
-//   await Project.create({ 
-//     title, 
-//     desc,
-//     href
-//   });
-//   return NextResponse.json({ status: 200 });
-// };
+export async function POST(request: Request, response: Response) {
+  const { 
+    title, 
+    desc, 
+    href
+  } = await request.json();
+  await connectMongoDB();
+  await Project.create({ 
+    title, 
+    desc,
+    href
+  });
+  return NextResponse.json({ status: 200 });
+};
 
-// export async function GET() {
-//   await connectMongoDB();
-//   const project = await Project.find();
-//   return NextResponse.json({ project })
-// };
+export async function GET() {
+  await connectMongoDB();
+  const project = await Project.find();
+  return NextResponse.json({ project })
+};
