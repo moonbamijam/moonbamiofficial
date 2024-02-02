075fb4d1528bf070c226f788bf45f9ddd2590f0c
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-02-02 23:59:33 +0800

    style: uncomment api topics

--- app/api/topics/[id]/route.ts
+++ app/api/topics/[id]/route.ts
@@ -1,26 +1,26 @@
-// import connectMongoDB from "@utilities/mongodb";
-// import About from "@models/topic";
-// import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
-// import { NextResponse } from "next/server";
+import connectMongoDB from "@utilities/mongodb";
+import About from "@models/topic";
+import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
+import { NextResponse } from "next/server";
 
-// export async function PUT(request: Request, { params }: Params) {
-//   const { id } = params;
-//   const { 
-//     title: title,
-//     desc: desc, 
-//   } = await request.json();
-//   await connectMongoDB();
-//   await About.findByIdAndUpdate(id, {
-//     title,
-//     desc,
-//     name,
-//   });
-//   return NextResponse.json({ message: "Updated" }, { status: 200 });
-// }
+export async function PUT(request: Request, { params }: Params) {
+  const { id } = params;
+  const { 
+    title: title,
+    desc: desc, 
+  } = await request.json();
+  await connectMongoDB();
+  await About.findByIdAndUpdate(id, {
+    title,
+    desc,
+    name,
+  });
+  return NextResponse.json({ message: "Updated" }, { status: 200 });
+}
 
-// export async function GET(request: Request, { params }: Params  ) {
-//   const { id } = params;
-//   await connectMongoDB();
-//   const about = await About.findOne({ _id: id });
-//   return NextResponse.json({ about }, { status: 200 });
-// };
+export async function GET(request: Request, { params }: Params  ) {
+  const { id } = params;
+  await connectMongoDB();
+  const about = await About.findOne({ _id: id });
+  return NextResponse.json({ about }, { status: 200 });
+};
--- app/api/topics/route.ts
+++ app/api/topics/route.ts
@@ -1,22 +1,22 @@
-// import connectMongoDB from "@utilities/mongodb";
-// import Topic from "@models/topic";
-// import { NextResponse } from "next/server";
+import connectMongoDB from "@utilities/mongodb";
+import Topic from "@models/topic";
+import { NextResponse } from "next/server";
 
-// export async function POST(request: Request, response: Response) {
-//   const { 
-//     title, 
-//     desc, 
-//   } = await request.json();
-//   await connectMongoDB();
-//   await Topic.create({ 
-//     title, 
-//     desc,
-//   });
-//   return NextResponse.json({message: "New topic created"})
-// };
+export async function POST(request: Request, response: Response) {
+  const { 
+    title, 
+    desc, 
+  } = await request.json();
+  await connectMongoDB();
+  await Topic.create({ 
+    title, 
+    desc,
+  });
+  return NextResponse.json({message: "New topic created"})
+};
 
-// export async function GET() {
-//   await connectMongoDB();
-//   const topics = await Topic.find();
-//   return NextResponse.json({ topics })
-// };
+export async function GET() {
+  await connectMongoDB();
+  const topics = await Topic.find();
+  return NextResponse.json({ topics })
+};
