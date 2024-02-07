fdc559bf1e4d3e747215f399c0d5aacdd02fcd52
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-02-07 10:27:58 +0800

    feat: add id in stack icon component

--- components/StackIcon.tsx
+++ components/StackIcon.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link'
 import { ReactNode } from 'react';
 
 type Props = {
+  id: string
   href: Url
   src: string | StaticImport
   alt: string 
@@ -14,9 +15,9 @@ type Props = {
 
 const StackIcon = (props: Props) => {
   return (
-    <Link href={ props.href } target='_blank' className="relative flex items-center justify-center">
-      <div className={`${ props.className } absolute w-[40px] h-[40px] md:w-[50px] md:h-[50px] lg:w-[60px] lg:h-[60px] `}></div>
-      <Image src={ props.src } alt={ props.alt } width={'50'} height={'50'} className="relative w-[50px] md:w-[60px] lg:w-[70px]" />
+    <Link id={ props.id } href={ props.href } target='_blank' className="relative flex items-center justify-center">
+      <div className={`w-[40px] h-[40px] md:w-[50px] md:h-[50px] lg:w-[60px] lg:h-[60px] ${ props.className } absolute `}></div>
+      <Image src={ props.src } alt={ props.alt } width={'1000'} height={'1000'} className="relative w-[50px] md:w-[60px] lg:w-[70px]" />
     </Link>
   )
 }
