2286cf09301077c9d68e4c5fcc0b511f9b3dd3a8
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-02-16 01:06:59 +0800

    fix: project content not showing in light mode

--- components/project/Project.tsx
+++ components/project/Project.tsx
@@ -7,8 +7,8 @@ const Project = (props: ProjectType) => {
     <div className="flex flex-col premium:flex-row-reverse gap-[15px] ">
       <Image src={ `${props.src}` } alt="" width={'3000'} height={'3000'} className="w-[400px] h-[220px] md:w-[640px] md:h-[480px] lg:w-[800px] lg:h-[400px] object-cover rounded-lg" />
       <div className="description">
-        <li className="text-3xl md:text-4xl lg:text-5xl font-bold mb-2 " >{ props.projectTitle }</li>
-        <p className="paragraph-format">
+        <li className="switch-text-color text-3xl md:text-4xl lg:text-5xl font-bold mb-2 " >{ props.projectTitle }</li>
+        <p className="switch-text-color paragraph-format">
           { props.projectDesc }
           <br /><br />
           You can visit the GitHub repository <Link href={ props.href } target='_blank' className="text-blue-400">here</Link>.
