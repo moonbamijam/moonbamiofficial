403f37e1457234f7094cbf230e27d7bd0420edfb
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-02-16 17:12:24 +0800

    perf: change li tag to h1 in project titles

--- components/project/Project.tsx
+++ components/project/Project.tsx
@@ -7,7 +7,7 @@ const Project = (props: ProjectType) => {
     <div className="flex flex-col premium:flex-row-reverse gap-[15px] ">
       <Image src={ `${props.src}` } alt="" width={'3000'} height={'3000'} className="w-[400px] h-[220px] md:w-[640px] md:h-[480px] lg:w-[800px] lg:h-[400px] object-cover rounded-lg" />
       <div className="description">
-        <li className="switch-text-color text-3xl md:text-4xl lg:text-5xl font-bold mb-2 " >{ props.projectTitle }</li>
+        <h1 className="switch-text-color text-3xl md:text-4xl lg:text-5xl font-bold mb-2 px-2" >{ props.projectTitle }</h1>
         <p className="switch-text-color paragraph-format">
           { props.projectDesc }
           <br /><br />
