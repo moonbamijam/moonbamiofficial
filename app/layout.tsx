c82c6f7aa1664514273a8fac256c9c9eddf90b9d
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-02-16 17:27:10 +0800

    feat: set dark mode by default

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" className="dark" style={{"colorScheme": "dark"}} suppressHydrationWarning>
       <body className={`${inter.className}`}>
         <Providers >
           <Header />
