8b2b7bf4ca2c4a6884eed7b0296d92c7c1f59e46
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-02-17 08:44:44 +0800

    feat: add new loading icon to loading ui & comps

--- app/music/loading.tsx
+++ app/music/loading.tsx
@@ -1,10 +1,10 @@
-import { ImSpinner2 } from "react-icons/im";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
 const Loading = () => {
   return (
     <div className="text-5xl lg:text-6xl absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  rounded-full w-max p-5 bg-rich-black dark:bg-transparent">
       <div className="animate-spin">
-        <ImSpinner2 />
+        <AiOutlineLoading3Quarters />
       </div>
     </div>
   )
