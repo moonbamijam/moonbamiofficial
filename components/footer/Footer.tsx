98cd9dc7edeac6efcc0aa86d21c31717b7c95762
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-02-18 08:51:24 +0800

    config: update hostname and formats for images

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import Anchor from "./Anchor";
 
+import Moonbami from '@public/moonbami.webp';
+
 const Footer = () => {
   const year = new Date()
   return (
@@ -9,7 +11,7 @@ const Footer = () => {
       <section id="footer" className="container relative bottom-0 pt-[100px] pb-[50px] px-[30px] md:px-[50px] xl:px-[100px] 2xl:px-[200px]">
         <div className="main flex flex-col lg:flex-row gap-[50px] items-center lg:items-start">
           <div className="brand flex flex-col gap-2 items-center">
-            <Image src={`${process.env.API_AVATAR}`} alt="" width={"3000"} height={"3000"} className="w-[100px] sm:w-[125px] md:w-[150px] xl:w-[175px] premium:w-[200px]  rounded-lg" />
+            <Image src={ Moonbami } alt="" width={"3000"} height={"3000"} className="w-[100px] sm:w-[125px] md:w-[150px] xl:w-[175px] premium:w-[200px]  rounded-lg" />
             <h1 className="switch-text-color font-bold text-3xl md:text-4xl lg:text-5xl ">Moonbami</h1>
           </div>
           <div className="content w-full flex justify-around">
--- next.config.js
+++ next.config.js
@@ -1,77 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    formats: ['image/avif', 'image/webp'],
     remotePatterns: [
       {
-        'hostname': 'mongodb.net'
-      },
-      {
-        'hostname': 'cdn.jsdelivr.net'
-      },
-      {
-        'hostname': 'www.facebook.com'
-      },
-      {
-        'hostname': 'scontent.fmnl35-1.fna.fbcdn.net'
-      },
-      {
-        'hostname': 'scontent.xx.fbcdn.net'
-      },
-      {
-        'hostname': 'www.javascript.com'
-      },
-      {
-        'hostname': 'www.typescriptlang.org'
-      },
-      {
-        'hostname': 'www.python.org'
-      },
-      {
-        'hostname': 'react.dev'
-      },
-      {
-        'hostname': 'nextjs.org'
-      },
-      {
-        'hostname': 'cdn.simpleicons.org'
-      },
-      {
-        'hostname': 'tailwindcss.com'
-      },
-      {
-        'hostname': 'www.djangoproject.com'
-      },
-      {
-        'hostname': 'sass-lang.com'
-      },
-      {
-        'hostname': 'web.dev'
-      },
-      {
-        'hostname': 'html.com'
-      },
-      {
-        'hostname': 'www.figma.com'
-      },
-      {
-        'hostname': 'www.adobe.com'
-      },
-      {
-        'hostname': 'git-scm.com'
-      },
-      {
-        'hostname': 'www.mongodb.com'
-      },
-      {
-        'hostname': 'www.mongodb.com'
-      },
-      {
-        'hostname': '64.media.tumblr.com'
-      },
-      {
-        'hostname': 'media1.tenor.com'
-      },
-  ],
+        protocol: 'https',
+        hostname: '**',
+        port: '',
+        pathname: '**'
+      }
+    ],
   }
 }
 
