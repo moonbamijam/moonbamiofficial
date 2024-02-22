cfe8b0644e6c5343c1d0c80e47514f71d9c46cad
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-02-22 20:23:55 +0800

    feat: add route link for each projects by id

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,20 +1,18 @@
 import { Metadata } from "next";
-import dynamic from "next/dynamic";
-
-// Components
-import StayTuned from "@components/StayTuned";
-import Section from "@components/wrappers/Section";
-import Title from "@components/texts/Title";
-import { ProjectType } from "@shared-types/project";
-import LoadingSpinner from "@components/LoadingSpinner";
 import { useFetch } from "@hooks/useFetch";
+import { ProjectType } from "@shared-types/project";
+import dynamic from "next/dynamic";
+import Title from "@components/texts/Title";
+import LoadingSpinner from "@components/LoadingSpinner";
+import Section from "@components/wrappers/Section";
 import BackToTop from "@components/BackToTop";
 import ProjectCard from "@components/ui/ProjectCard";
-const Footer = dynamic(() => import('@components/footer/Footer'));
+
+const Footer = dynamic(() => import("@components/footer/Footer"));
 
 export const metadata: Metadata = {
-  title: "Projects"
-}
+  title: "Projects",
+};
 
 const ProjectsPage = async () => {
   const { projects } = await useFetch("/api/projects");
@@ -23,11 +21,27 @@ const ProjectsPage = async () => {
       <div className="container relative">
         <BackToTop id={"#project"} />
         <Section id={"project"} contentClassName={"gap-[150px]"}>
-          <Title name={"projects"} desc={"These are the projects I made all throughout my coding career. I am happy to present it to you."} /> 
-          <div className="projects flex flex-col gap-[100px]">
-            {projects ? projects.map((project: ProjectType) => (
-              <ProjectCard key={ project._id } src={ project.src } alt={ project.alt } title={ project.title } desc={ project.desc } website={ project.website } github={ project.github } />
-            )) : (
+          <Title
+            name={"projects"}
+            desc={
+              "These are the projects I made all throughout my coding career. I am happy to present it to you."
+            }
+          />
+          <div className="projects grid lg:grid-cols-2 2xl:grid-cols-3 gap-[50px]">
+            {projects ? (
+              projects.map((project: ProjectType) => (
+                <ProjectCard
+                  key={project._id}
+                  href={`projects/${project._id}`}
+                  src={project.src}
+                  alt={project.alt}
+                  title={project.title}
+                  desc={project.desc}
+                  website={project.website}
+                  github={project.github}
+                />
+              ))
+            ) : (
               <LoadingSpinner />
             )}
           </div>
@@ -35,7 +49,7 @@ const ProjectsPage = async () => {
       </div>
       <Footer />
     </>
-  )
+  );
 };
 
 export default ProjectsPage;
