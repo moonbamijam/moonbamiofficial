fb8488ba0040adc20675170966fae63ce8ade787
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-02-22 20:21:57 +0800

    feat: add dropdown component

---
+++ components/ui/Dropdown.tsx
@@ -0,0 +1,16 @@
+type Props = {
+  children?: React.ReactNode;
+};
+
+const Dropdown = ({ children }: Props) => {
+  return (
+    <nav
+      id="main"
+      className="absolute text-base top-[60px] flex flex-col gap-2 mt-2 px-2 py-8 rounded-xl bg-white dark:bg-primary border border-transparent dark:border-white"
+    >
+      {children}
+    </nav>
+  );
+};
+
+export default Dropdown;
