a62da99e7ed7b6464135f5b0715238dfeeff0c04
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-02-22 20:22:23 +0800

    feat: add new styling to back to top button

--- components/BackToTop.tsx
+++ components/BackToTop.tsx
@@ -25,7 +25,7 @@ const BackToTop = (props: Props) => {
   }, [])
   return (
     <button aria-label="back-to-top" onClick={() => handleScrollIntoView( `${ props.id }`)} className={`${ backToTop } fixed text-4xl md:text-5xl z-[100] right-8 sm:right-12 sm:bottom-12 bottom-8 lg:right-24 lg:bottom-16 flex flex-col items-center gap-2 animate-bounce`}>
-      <div className="rounded-full bg-black dark:bg-transparent">
+      <div className="rounded-full">
         <IoIosArrowDropupCircle />
       </div>
       <h1 className=" text-black dark:text-white text-base hidden lg:block ">Back to Top</h1>
