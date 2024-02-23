010f2ffaff6052e3fb68c9e156fecf1d74fb6847
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-02-24 01:32:39 +0800

    feat: add new text color in not found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -6,7 +6,7 @@ const NotFound = () => {
     <div id="" className="container relative">
       <section className="content pb-[300px] pt-[200px] lg:pt-[250px] flex flex-col gap-10 items-center justify-center">
         <h1 className="switch-text-color font-bold text-lg md:text-xl lg:text-3xl xl:text-7xl text-center">Page not found! 
-          <h2 className="switch-text-color font-normal text-lg">Go back to <Link href="/" className="text-blue-400 capitalize hover:underline inline">home</Link></h2>
+          <h2 className="switch-text-color font-normal text-lg">Go back to <Link href="/" className="text-highlight capitalize hover:underline inline">home</Link></h2>
         </h1>
         <Image src={`${process.env.API_NOT_FOUND}`} alt="" width={"3000"} height={"3000"} className="w-[150px] h-[150px] lg:w-[300px] lg:h-[300px] object-cover rounded-full" />
       </section>
