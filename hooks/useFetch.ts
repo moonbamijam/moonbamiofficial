9448fc4e0c31ad92c903abdf799d6b47b820d23e
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-02-24 23:46:39 +0800

    perf: remove excess await calls in use fetch

--- hooks/useFetch.ts
+++ hooks/useFetch.ts
@@ -1,13 +1,7 @@
 export const useFetch = async (url: string) => {
-  try {
-    const response = await fetch(process.env.URL + url , {
-      cache: 'no-store',
-      method: 'GET'
-    });
-    const data = await response.json();
-    return await data;
-
-  } catch (error) {
-    console.log("Error occured: ", error);
-  }
+  const response = await fetch(process.env.URL + url, {
+    cache: "no-store",
+    method: "GET",
+  });
+  return response.json();
 };
