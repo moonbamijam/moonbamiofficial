91be37dd59cf86aad0256bb16b60832564355892
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-02-27 21:41:41 +0800

    feat: add letter v in before version number

---
+++ components/ui/Version.tsx
@@ -0,0 +1,12 @@
+import Package from "@package.json";
+import { IoIosGitBranch } from "react-icons/io";
+
+const Version = () => {
+  return (
+    <div className="flex items-center gap-1">
+      <IoIosGitBranch />v{Package.version}
+    </div>
+  );
+};
+
+export default Version;
