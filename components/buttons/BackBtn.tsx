2dbf7c911d3b3ff00a71f3999da4fc65593bfecb
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-03-01 16:26:41 +0800

    feat: add new back button component

---
+++ components/buttons/BackBtn.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import { FaBackwardStep } from "react-icons/fa6";type Props = {
+  href: string;
+  extraClass?: string;
+};
+
+const BackBtn = ({ href, extraClass }: Props) => {
+  return (
+    <Link
+      href={href}
+      className={`${extraClass} flex justify-center items-center gap-1 text-black [&>svg>path]:text-black dark:text-white dark:[&>svg>path]:text-white hover:text-white [&>svg>path]:hover:text-white button-format border-2 border-gray-500 hover:bg-highlight hover:border-highlight capitalize`}
+    >
+      <FaBackwardStep /> back
+    </Link>
+  );
+};
+
+export default BackBtn;
