32dd181f52de598282f8770343eab9e065b18458
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-03-01 09:48:54 +0800

    feat: add container class for section component

--- components/wrappers/Section.tsx
+++ components/wrappers/Section.tsx
@@ -1,22 +1,27 @@
-import { ReactNode } from "react"
+import { ReactNode } from "react";
 
 type Props = {
-  id: string
-  image?: React.ReactNode
-  children: React.ReactNode
-  sectionClassName?: string
-  contentClassName?: string
-}
+  id: string;
+  image?: React.ReactNode;
+  children: React.ReactNode;
+  sectionClassName?: string;
+  contentClassName?: string;
+};
 
 const Section = (props: Props) => {
   return (
-    <section id={ props.id } className={`w-full relative py-[100px] ${ props.sectionClassName }`}>
-      { props.image }
-      <div className={`content relative flex flex-col items-center px-[30px] ${ props.contentClassName }`}>
-        { props.children }
+    <section
+      id={props.id}
+      className={`container w-full relative py-[100px] ${props.sectionClassName}`}
+    >
+      {props.image}
+      <div
+        className={`content relative flex flex-col items-center px-[30px] ${props.contentClassName}`}
+      >
+        {props.children}
       </div>
     </section>
-  )
-}
+  );
+};
 
 export default Section;
