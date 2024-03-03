b4a386e53bdcc2148bc3d2c8f056da93072ab5a7
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-03-03 18:49:46 +0800

    refactor: change dir dest for next-pwa

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const withPWA = require("@ducanh2912/next-pwa").default({
-  dest: "pwa",
+  dest: "public",
   cacheOnFrontEndNav: true,
   aggressiveFrontEndNavCaching: true,
   reloadOnOnline: true,
