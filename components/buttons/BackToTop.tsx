0935a103896639cb7b9df7038121a9a923e5cbe6
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-03-03 23:25:46 +0800

    feat: update icon in back to top button component

--- components/buttons/BackToTop.tsx
+++ components/buttons/BackToTop.tsx
@@ -1,29 +1,30 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { FiArrowUpCircle } from "react-icons/fi";
+import { BiSolidUpArrow } from "react-icons/bi";
 
 type Props = {
-  id?: string;
+  id: string;
+};
+
+const handleScrollIntoView = (scrollInto: string) => {
+  const profileSection = document.querySelector(scrollInto);
+  if (profileSection) profileSection.scrollIntoView({ behavior: "smooth" });
 };
 
 const BackToTop = ({ id }: Props) => {
   const [backToTop, setBackToTop] = useState("hide-back-to-top");
 
-  const handleScrollIntoView = (scrollInto: string) => {
-    const profileSection = document.querySelector(scrollInto);
-    if (profileSection) profileSection.scrollIntoView({ behavior: "smooth" });
-  };
-
   const handleBackToTop = () => {
     if (window.scrollY > 500) return setBackToTop("show-back-to-top");
-    else if (window.scrollY < 1) return setBackToTop("hide-back-to-top");
+    else if (window.scrollY < 15) return setBackToTop("hide-back-to-top");
   };
 
   useEffect(() => {
     window.addEventListener("scroll", handleBackToTop);
     return () => window.removeEventListener("scroll", handleBackToTop);
   }, []);
+
   return (
     <button
       aria-label="back-to-top"
@@ -31,7 +32,7 @@ const BackToTop = ({ id }: Props) => {
       className={`${backToTop} fixed text-4xl md:text-5xl z-[100] right-8 sm:right-12 sm:bottom-12 bottom-8 lg:right-24 lg:bottom-16 flex flex-col items-center gap-2 animate-bounce`}
     >
       <div className="rounded-full">
-        <FiArrowUpCircle />
+        <BiSolidUpArrow />
       </div>
       <h1 className=" text-black dark:text-white text-base hidden lg:block ">
         Back to Top
