b82f4d23fd5ddbbd967c200c17f309712788a90d
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-03-03 18:40:12 +0800

    feat: add margin right to dropdown in header

--- components/ui/Dropdown.tsx
+++ components/ui/Dropdown.tsx
@@ -6,7 +6,7 @@ const Dropdown = ({ children }: Props) => {
   return (
     <nav
       id="main"
-      className="absolute text-base top-[60px] flex flex-col gap-2 mt-2 px-2 py-8 rounded-xl bg-white dark:bg-primary border border-transparent dark:border-white"
+      className="absolute text-base top-[60px] flex flex-col gap-2 mt-2 mr-2 px-2 py-8 rounded-xl bg-white dark:bg-primary border border-transparent dark:border-white"
     >
       {children}
     </nav>
