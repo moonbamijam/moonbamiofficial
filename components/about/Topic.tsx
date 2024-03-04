cdd385bf74f7545147f9589aa691738d3cad437a
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-03-04 23:19:17 +0800

    perf: remove unused utility class

--- components/about/Topic.tsx
+++ components/about/Topic.tsx
@@ -3,10 +3,10 @@ import { TopicType } from "@shared-types/topic";
 const Topic = ({ title, desc }: TopicType) => {
   return (
     <div className="topic flex flex-col items-center">
-      <h1 className="switch-text-color capitalize text-4xl md:text-5xl text-center font-bold mb-2">
+      <h1 className="capitalize text-4xl md:text-5xl text-center font-bold mb-2">
         {title}
       </h1>
-      <p className="switch-text-color paragraph-format">{desc}</p>
+      <p className="paragraph-format">{desc}</p>
     </div>
   );
 };
