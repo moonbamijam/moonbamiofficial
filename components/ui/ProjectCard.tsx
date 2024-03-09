7633f185e8322f4382f59f4d23309e0f1155209c
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-03-09 10:47:14 +0800

    feat: remove border to projects and add new shadow

--- components/ui/ProjectCard.tsx
+++ components/ui/ProjectCard.tsx
@@ -1,7 +1,7 @@
 import { ProjectType } from "@shared-types/project";
 import Image from "next/image";
 import Link from "next/link";
-import DefaultBanner from "@assets/default-banner.webp"
+import DefaultBanner from "@assets/default-banner.webp";
 import GitHubBtn from "@components/buttons/GitHubBtn";
 import WebsiteBtn from "@components/buttons/WebsiteBtn";
 
@@ -15,7 +15,7 @@ const ProjectCard = ({
   github,
 }: ProjectType) => {
   return (
-    <div className="card relative w-[400px] rounded-xl flex flex-col bg-transparent overflow-hidden">
+    <div className="cursor-pointer hover:shadow-gray-500 hover:shadow-lg hover:-translate-y-2 relative w-[400px] rounded-xl flex flex-col overflow-hidden">
       <Link href={href}>
         <Image
           src={src ? `${src}` : DefaultBanner}
@@ -25,7 +25,7 @@ const ProjectCard = ({
           loading="lazy"
           placeholder="blur"
           blurDataURL="URL"
-          className="w-full h-[200px] object-cover"
+          className="w-full h-[200px] rounded-xl object-cover"
         />
         <div className="details px-[30px] py-[30px]">
           <h1 className="truncate text-2xl font-bold px-2">{title}</h1>
