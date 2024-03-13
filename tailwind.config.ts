886befeab52eec743c038cafd370468bc1729ada
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-03-14 00:31:00 +0800

    refactor: change rgb color values to hex

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,32 +1,31 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-  darkMode: 'class',
+  darkMode: "class",
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        'opaque-rb': 'rgba(3, 0, 28, 0.8)',
-        primary: 'rgb(var(--primary))',
-        secondary: 'rgb(var(--secondary))',
-        highlight: 'rgb(var(--highlight))',
+        primary: "#020202",
+        secondary: "#2D3436",
+        highlight: "#7BACEC",
       },
       container: {
         center: true,
       },
       screens: {
-        'xs': '475px',
-        'premium': '1920px',
+        xs: "475px",
+        premium: "1920px",
       },
       boxShadow: {
-        'gradient': '0px 0px 100px 100px rgba(3, 0, 28, 0.3)',
+        gradient: "0px 0px 100px 100px rgba(3, 0, 28, 0.3)",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
