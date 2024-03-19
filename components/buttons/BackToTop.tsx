5638cfb340823f34eae1062f5a473a2f87c12036
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-03-19 22:17:55 +0800

    fix: random bug in back to top btn component

--- components/buttons/BackToTop.tsx
+++ components/buttons/BackToTop.tsx
@@ -7,20 +7,19 @@ type Props = {
   id: string;
 };
 
-const handleScrollIntoView = (scrollInto: string) => {
-  const profileSection = document.querySelector(scrollInto);
-  if (profileSection) profileSection.scrollIntoView({ behavior: "smooth" });
-};
-
 const BackToTop = ({ id }: Props) => {
   const [backToTop, setBackToTop] = useState("opacity-0 invisible");
 
-  const handleBackToTop = () => {
-    if (window.scrollY > 500) return setBackToTop("opacity-100 visible");
-    else if (window.scrollY < 15) return setBackToTop("opacity-0 invisible");
+  const handleScrollIntoView = (scrollInto: string) => {
+    const profileSection = document.querySelector(scrollInto);
+    if (profileSection) profileSection.scrollIntoView({ behavior: "smooth" });
   };
 
   useEffect(() => {
+    const handleBackToTop = () => {
+      if (window.scrollY > 500) return setBackToTop("opacity-100 visible");
+      else if (window.scrollY < 15) return setBackToTop("opacity-0 invisible");
+    };
     window.addEventListener("scroll", handleBackToTop);
     return () => window.removeEventListener("scroll", handleBackToTop);
   }, []);
