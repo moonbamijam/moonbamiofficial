0f3ac2b74d494286059a06bca1fed2601d9330f3
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-05-02 23:14:05 +0800

    feat: new color styling to loading spiner ux component

--- components/ux/LoadingSpinner.tsx
+++ components/ux/LoadingSpinner.tsx
@@ -2,28 +2,26 @@ import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
 type LoadingSpinnerProps = {
   size?: string;
-  textSize?: string;
-  textSizes?: string;
+  fontSize?: string;
+  fontSizes?: string;
 };
 
 export default function LoadingSpinner({
   size,
-  textSize,
-  textSizes,
+  fontSize,
+  fontSizes,
 }: LoadingSpinnerProps) {
   return (
     <div
-      className={`relative w-[${size}] h-[${size}] ${
-        "text-" + [textSize]
-      } ${textSizes} flex items-center justify-center rounded-full`}
+      className={`relative w-[${size}] h-[${size}] flex items-center justify-center rounded-full [&>svg>path]:text-[${fontSize}] ${fontSizes}`}
     >
       <div className="absolute opacity-30">
-        <AiOutlineLoading3Quarters className="[&>path]:text-white" />
+        <AiOutlineLoading3Quarters className="[&>path]:text-surface" />
       </div>
       <div className="absolute opacity-30 rotate-90">
-        <AiOutlineLoading3Quarters className="[&>path]:text-white" />
+        <AiOutlineLoading3Quarters className="[&>path]:text-surface" />
       </div>
-      <AiOutlineLoading3Quarters className="animate-spin duration-500 [&>path]:text-highlight" />
+      <AiOutlineLoading3Quarters className="animate-spin duration-500 [&>path]:text-primary [&>path]:dark:text-primary-light" />
     </div>
   );
 }
