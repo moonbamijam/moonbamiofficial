a6bbefc3e7e8597d4e96043af1534431d1e1eb00
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-05-02 10:47:30 +0800

    refactor: change not-found.tsx to named function

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,17 +1,30 @@
 import Image from "next/image";
 import Link from "next/link";
 
-const NotFound = () => {
+export default function NotFound() {
   return (
-    <div id="" className="container relative">
+    <div className="container relative">
       <section className="content pb-[300px] pt-[200px] lg:pt-[250px] flex flex-col gap-10 items-center justify-center">
-        <h1 className="switch-text-color font-bold text-lg md:text-xl lg:text-3xl xl:text-7xl text-center">Page not found! 
-          <h2 className="switch-text-color font-normal text-lg">Go back to <Link href="/" className="text-highlight capitalize hover:underline inline">home</Link></h2>
+        <h1 className="switch-text-color font-bold text-lg md:text-xl lg:text-3xl xl:text-7xl text-center">
+          Page not found!
+          <h2 className="switch-text-color font-normal text-lg">
+            Go back to
+            <Link
+              href="/"
+              className="text-highlight capitalize hover:underline inline"
+            >
+              home
+            </Link>
+          </h2>
         </h1>
-        <Image src={`${process.env.API_NOT_FOUND}`} alt="" width={"3000"} height={"3000"} className="w-[150px] h-[150px] lg:w-[300px] lg:h-[300px] object-cover rounded-full" />
+        <Image
+          src={`${process.env.API_NOT_FOUND}`}
+          alt=""
+          width={"3000"}
+          height={"3000"}
+          className="w-[150px] h-[150px] lg:w-[300px] lg:h-[300px] object-cover rounded-full"
+        />
       </section>
     </div>
-  )
+  );
 }
-
-export default NotFound;
