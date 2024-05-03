c17e1f71a64747ac1396793b651a943b032816e4
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-05-04 02:31:51 +0800

    feat: new styling to dropdown ui component

--- components/ui/Dropdown.tsx
+++ components/ui/Dropdown.tsx
@@ -6,7 +6,7 @@ export default function Dropdown({ children }: Props) {
   return (
     <nav
       id="main"
-      className="absolute text-base top-[60px] flex flex-col gap-2 mt-2 mr-2 px-2 py-8 rounded-xl bg-white dark:bg-primary border border-transparent dark:border-white"
+      className="absolute text-base top-[60px] flex flex-col gap-2 mt-2 mr-2 px-2 py-8 rounded-xl bg-surface dark:bg-dm-surface border border-transparent"
     >
       {children}
     </nav>
