96788d8838377d383090078ff73438665951cf87
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-05-04 02:23:19 +0800

    fix: argument values to loading ui

--- app/loading.tsx
+++ app/loading.tsx
@@ -3,7 +3,7 @@ import LoadingSpinner from "@components/ux/LoadingSpinner";
 const Loading = () => {
   return (
     <div className="text-5xl lg:text-6xl absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  rounded-full w-max p-5">
-      <LoadingSpinner size="100px" textSize="6xl" />
+      <LoadingSpinner size="100px" fontSize="64px" />
     </div>
   );
 };
