e7615b458dcb693ba595728aaf6530046605795c
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-05-04 02:32:55 +0800

    config: move colors to theme instead of extending it

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import colors, { transparent } from "tailwindcss/colors";
 
 const config: Config = {
   darkMode: "class",
@@ -8,23 +9,30 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      black: colors.black,
+      white: colors.white,
+      transparent: colors.transparent,
+      primary: "rgb(var(--primary) / <alpha-value>)",
+      "primary-dark": "rgb(var(--primary-dark) / <alpha-value>)",
+      "primary-light": "rgb(var(--primary-light) / <alpha-value>)",
+      background: "rgb(var(--background) / <alpha-value>)",
+      "dark-background": "#121212",
+      surface: "rgb(var(--surface) / <alpha-value>)",
+      "dm-surface": "#1F1F1F",
+      "dm-surface-dark": "#151515",
+      "dm-surface-light": "#2D2D2D",
+      error: "rgb(var(--error) / <alpha-value>)",
+      "on-primary": "rgb(var(--on-primary) / <alpha-value>)",
+      "on-primary-dark": "rgb(var(--on-primary-dark) / <alpha-value>)",
+      "on-secondary": "rgb(var(--on-secondary) / <alpha-value>)",
+      "on-background": "rgb(var(--on--background) / <alpha-value>)",
+      "on-surface": "rgb(var(--on-surface) / <alpha-value>)",
+      "on-error": "rgb(var(--on-error) / <alpha-value>)",
+      // foreign colors - colors that are not part of the project
+      "color-github": "#2dba4e",
+    },
     extend: {
-      colors: {
-        primary: "rgb(var(--primary) / <alpha-value>)",
-        "primary-dark": "rgb(var(--primary-dark) / <alpha-value>)",
-        "primary-light": "rgb(var(--primary-light) / <alpha-value>)",
-        background: "rgb(var(--background) / <alpha-value>)",
-        "dark-background": "#121212",
-        surface: "rgb(var(--surface) / <alpha-value>)",
-        "dark-surface": "#1F1F1F",
-        error: "rgb(var(--error) / <alpha-value>)",
-        "on-primary": "rgb(var(--on-primary) / <alpha-value>)",
-        "on-primary-dark": "rgb(var(--on-primary-dark) / <alpha-value>)",
-        "on-secondary": "rgb(var(--on-secondary) / <alpha-value>)",
-        "on-background": "rgb(var(--on--background) / <alpha-value>)",
-        "on-surface": "rgb(var(--on-surface) / <alpha-value>)",
-        "on-error": "rgb(var(--on-error) / <alpha-value>)",
-      },
       container: {
         center: true,
       },
