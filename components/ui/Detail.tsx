2dffe3ac960b36455a78a463338680b51d82a102
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-05-04 02:30:39 +0800

    refactor: remove unused utility class to detail ui component

--- components/ui/Detail.tsx
+++ components/ui/Detail.tsx
@@ -6,12 +6,12 @@ export default function Detail({ label, detail }: Props) {
   return (
     <div className="details flex">
       <div className="label flex">
-        <span className="switch-text-color capitalize w-[100px] lg:w-[115px] xl:w-[130px] text-format">
+        <span className="capitalize w-[100px] lg:w-[115px] xl:w-[130px] text-format">
           {label}
         </span>
         <span>:</span>
       </div>
-      <span className="switch-text-color capitalize flex flex-wrap w-[200px] lg:w-[300px] xl:w-[400px] text-format">
+      <span className="capitalize flex flex-wrap w-[200px] lg:w-[300px] xl:w-[400px] text-format">
         {detail}
       </span>
     </div>
