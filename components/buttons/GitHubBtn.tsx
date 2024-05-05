6535ce07b7c3af79d28e0baa749bc4fcd062724e
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-05-05 23:45:14 +0800

    feat: add conditional text to github btn component

--- components/buttons/GitHubBtn.tsx
+++ components/buttons/GitHubBtn.tsx
@@ -1,7 +1,12 @@
 import Button from "@components/ui/Button";
 import { FaGithub } from "react-icons/fa6";
 
-export default function GitHubBtn({ href }: { href: string }) {
+type GitHubBtnProps = {
+  href: string;
+  text?: string;
+};
+
+export default function GitHubBtn({ href, text }: GitHubBtnProps) {
   return (
     <Button
       buttonType="link"
@@ -10,7 +15,7 @@ export default function GitHubBtn({ href }: { href: string }) {
       target="_blank"
       ariaLabel="GitHub"
       icon={<FaGithub />}
-      text="GitHub"
+      text={text ? text : "GitHub"}
       customStyles="border-primary [&>svg>path]:text-black"
       hoverStyles="hover:bg-color-github hover:border-color-github hover:text-on-primary [&>svg>path]:hover:text-on-primary"
       darkModeStyles="[&>svg>path]:dark:text-white"
