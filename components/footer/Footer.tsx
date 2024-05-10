407d578fe6e82602bccb218ec52254c726e447bd
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-05-10 22:32:02 +0800

    feat: minimal ui improvement to footer

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,92 +1,45 @@
-import { SiAnilist } from "react-icons/si";
-import { FaFacebook } from "react-icons/fa6";
-import { FaInstagram } from "react-icons/fa6";
-import { FaGithub } from "react-icons/fa6";
-import { FaXTwitter } from "react-icons/fa6";
-import { FaRedditAlien } from "react-icons/fa6";
-import { FaYoutube } from "react-icons/fa6";
-import SocMedIcon from "./SocMedIcon";
+import { socialMedias } from "@lib/socialMedias";
 import WaveOpacity from "@components/curves/WaveOpacity";
+import Version from "@components/ui/Version";
+import SocMedIcon from "./SocMedIcon";
 import Projects from "./Projects";
 import Navigate from "./Navigate";
-import Quote from "@components/ui/Quote";
-
-const NavLinks = [
-  { id: 1, name: "home", path: "/" },
-  { id: 2, name: "about", path: "/about" },
-  { id: 3, name: "projects", path: "/projects" },
-  { id: 4, name: "timeline", path: "/timeline" },
-  { id: 5, name: "more", path: "/more" },
-];
+import Connect from "./Connect";
 
 export default function Footer() {
   const year = new Date();
   return (
-    <>
-      <section id="footer" className="container relative bottom-0">
-        <WaveOpacity />
-        <div className="py-[64px] px-[8vw] text-on-primary bg-primary dark:bg-primary-dark">
-          <div className="flex flex-col lg:flex-row items-center mb-8">
-            <h1 className="text-3xl lg:text-4xl font-bold text-center tracking-tight mb-8">
-              Let&apos;s build from here!
-            </h1>
-            <div className="w-full md:w-[80%] flex justify-evenly">
-              <Projects />
-              <Navigate />
-            </div>
-          </div>
-          <hr className="opacity-50" />
-          <div className="w-full mt-8 flex flex-col justify-evenly items-center gap-16">
-            <div className="flex flex-col items-center gap-y-8">
-              <Quote
-                quote="The greatest successes come from having the freedom to fail."
-                author="Mark Zuckerberg"
-                customStyles="sm:max-w-[400px] lg:max-w-max"
-              />
-              <div className="max-w-[80%] flex justify-evenly gap-2">
-                <SocMedIcon
-                  href={"https://anilist.co/user/Moonbami/"}
-                  ariaLabel={"AniList"}
-                  icon={<SiAnilist />}
-                />
-                <SocMedIcon
-                  href={"https://www.facebook.com/moonbamijam"}
-                  ariaLabel={"Facebook"}
-                  icon={<FaFacebook />}
-                />
-                <SocMedIcon
-                  href={"https://www.instagram.com/moonbamidesu/"}
-                  ariaLabel={"Instagram"}
-                  icon={<FaInstagram />}
-                />
-                <SocMedIcon
-                  href={"https://github.com/MoonbamiOfficial"}
-                  ariaLabel={"GitHub"}
-                  icon={<FaGithub />}
-                />
-                <SocMedIcon
-                  href={"https://twitter.com/MoonbamiOffcl"}
-                  ariaLabel={"X"}
-                  icon={<FaXTwitter />}
-                />
-                <SocMedIcon
-                  href={"https://www.reddit.com/user/MoonbamiOfficial/"}
-                  ariaLabel={"Reddit"}
-                  icon={<FaRedditAlien />}
-                />
-                <SocMedIcon
-                  href={"https://www.youtube.com/@Moonbami"}
-                  ariaLabel={"YouTube"}
-                  icon={<FaYoutube />}
-                />
-              </div>
-            </div>
-            <p className="min-w-[400px] text-center">
-              &#169; {year.getFullYear().toString()} Moonbami. Philippines.
-            </p>
+    <section id="footer" className="container relative bottom-0">
+      <WaveOpacity />
+      <div className="py-[64px] px-[8vw] text-on-primary bg-primary dark:bg-primary-dark">
+        <div className="flex flex-col lg:flex-row justify-between items-center mb-8">
+          <h1 className="text-3xl xl:text-4xl font-bold text-center lg:text-left tracking-tight mb-8">
+            Let&apos;s build from here!
+          </h1>
+          <div className="w-full lg:w-[60%] 2xl:w-[70%] flex justify-evenly">
+            <Projects />
+            <Connect />
+            <Navigate />
           </div>
         </div>
-      </section>
-    </>
+        <hr className="opacity-50" />
+        <div className="w-full mt-8 flex flex-col md:flex-row-reverse md:justify-around items-center gap-y-4">
+          <Version customStyles="min-w-[300px] justify-center" />
+          <div className="min-w-[300px] hidden xl:flex justify-evenly gap-2">
+            {socialMedias.map((socialMedia, id) => (
+              <SocMedIcon
+                key={id}
+                href={socialMedia.href}
+                ariaLabel={socialMedia.text}
+                icon={socialMedia.icon}
+              />
+            ))}
+          </div>
+          <p className="min-w-[300px] text-center md:text-left">
+            &#169; {year.getFullYear().toString()} Moonbami. Philippines.{" "}
+          </p>
+        </div>
+      </div>
+    </section>
   );
 }
