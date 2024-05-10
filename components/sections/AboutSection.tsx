679a5fdcef2b61129cec2e8870aa3f59a8a4cc81
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-05-10 22:27:44 +0800

    feat: add image credits to image in about page

--- components/sections/AboutSection.tsx
+++ components/sections/AboutSection.tsx
@@ -1,10 +1,11 @@
+import { useFetch } from "@hooks/useFetch";
+import { AboutType } from "@lib/types";
 import Picture from "@components/common/Picture";
 import Section from "@components/common/Section";
 import Heading from "@components/ui/Heading";
 import AboutUi from "@components/ui/AboutUi";
-import LoadingSpinner from "@components/ux/LoadingSpinner";
-import { useFetch } from "@hooks/useFetch";
-import { AboutType } from "@lib/types";
+import LoadingSpinner from "@components/ui/LoadingSpinner";
+import ImageCredits from "@components/ui/ImageCredits";
 
 export default async function AboutSection() {
   const { abouts } = await useFetch("/api/abouts");
@@ -30,14 +31,18 @@ export default async function AboutSection() {
   };
 
   return (
-    <Section id={"about"} contentStyles={"gap-[100px]"}>
+    <Section id={"about"}>
       <Heading
         name={"about me"}
         desc={"Let's dive deeper of who am I behind the social media."}
       />
       <div className="about-me w-full flex flex-col lg:flex-row justify-between items-center lg:items-start xl:justify-evenly ">
-        <div className="relative w-[300px] h-[300px] lg:w-[400px] lg:h-[400px] 2xl:w-[500px] 2xl:h-[500px] aspect-square shadow-md hover:shadow-primary-dark mb-4 rounded-lg overflow-hidden">
+        <div className="relative w-[300px] h-[300px] lg:w-[400px] lg:h-[400px] 2xl:w-[500px] 2xl:h-[500px] aspect-square shadow-md hover:shadow-primary-dark mb-4 rounded-lg [&>span]:hover:opacity-70 overflow-hidden">
           <Picture src={"me_x4brue"} alt={"Jam Moonbami"} isPriority={true} />
+          <ImageCredits
+            creditsTo="Maki"
+            creditLink="https://www.instagram.com/prettyboimaks/"
+          />
         </div>
         <div className="about-me flex flex-col gap-[30px] ">
           {renderAbout()}
