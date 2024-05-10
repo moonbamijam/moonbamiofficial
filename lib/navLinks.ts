321f0e33d277e68cb5d0f187d0d3313c7e97ece7
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-05-10 22:29:50 +0800

    feat: add new project specific array of objects to lib

---
+++ lib/navLinks.ts
@@ -0,0 +1,7 @@
+export const NavLinks = [
+  { id: 1, name: "home", path: "/" },
+  { id: 2, name: "about", path: "/about" },
+  { id: 3, name: "projects", path: "/projects" },
+  { id: 4, name: "timeline", path: "/timeline" },
+  { id: 5, name: "more", path: "/more" },
+];
---
+++ lib/socialMedias.tsx
@@ -0,0 +1,47 @@
+import { SiAnilist } from "react-icons/si";
+import {
+  FaFacebook,
+  FaInstagram,
+  FaGithub,
+  FaXTwitter,
+  FaRedditAlien,
+  FaYoutube,
+} from "react-icons/fa6";
+
+export const socialMedias = [
+  {
+    href: "https://anilist.co/user/Moonbami/",
+    text: "AniList",
+    icon: <SiAnilist />,
+  },
+  {
+    href: "https://www.facebook.com/moonbamijam",
+    text: "Facebook",
+    icon: <FaFacebook />,
+  },
+  {
+    href: "https://www.instagram.com/moonbamidesu/",
+    text: "Instagram",
+    icon: <FaInstagram />,
+  },
+  {
+    href: "https://github.com/MoonbamiOfficial",
+    text: "GitHub",
+    icon: <FaGithub />,
+  },
+  {
+    href: "https://twitter.com/MoonbamiOffcl",
+    text: "Twitter X",
+    icon: <FaXTwitter />,
+  },
+  {
+    href: "https://www.reddit.com/user/MoonbamiOfficial/",
+    text: "Reddit",
+    icon: <FaRedditAlien />,
+  },
+  {
+    href: "https://www.youtube.com/@Moonbami",
+    text: "YouTube",
+    icon: <FaYoutube />,
+  },
+];
