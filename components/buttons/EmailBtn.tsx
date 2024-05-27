b33e35457aba016579ad10b30ee33590b6051169
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-05-27 17:42:38 +0800

    feat: new icon to email btn in landing page

--- components/buttons/EmailBtn.tsx
+++ components/buttons/EmailBtn.tsx
@@ -1,4 +1,4 @@
-import { MdEmail } from "react-icons/md";
+import { SiGmail } from "react-icons/si";
 import Button from "@components/ui/Button";
 
 type EmailBtnProps = {
@@ -13,7 +13,7 @@ export default function EmailBtn({ href }: EmailBtnProps) {
       target="_blank"
       id="email-btn"
       text="email"
-      icon={<MdEmail />}
+      icon={<SiGmail />}
       customStyles="border-primary bg-primary text-on-primary"
       hoverStyles="hover:bg-primary-dark hover:border-primary-dark"
       mixedStyles="dark:hover:border-primary-dark dark:hover:bg-primary-dark"
