d72e95b15cbb01578ffeb61bcb8b881eb9a01415
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-07-13 22:25:36 +0800

    feat: add randomize quote ui component (#35)

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import Picture from "@components/common/Picture";
 import LandingSkeleton from "@components/skeletons/LandingSkeleton";
 import Quote from "@components/ui/Quote";
 import ImageCredits from "@components/ui/ImageCredits";
+import RandomizeQuote from "@components/ui/RandomizeQuote";
 
 const HomePage = async () => {
   return (
@@ -13,11 +14,11 @@ const HomePage = async () => {
         <section id="home" className="h-screen">
           <div className="w-full h-full relative px-[5vw] flex flex-col justify-center items-center xl:flex-row xl:justify-around 2xl:justify-evenly gap-y-12">
             <div className="flex flex-col items-center xl:items-start gap-6">
-              <h1 className="text-center xl:max-w-[600px] 2xl:max-w-none xl:text-left text-5xl md:text-7xl lg:text-8xl px-1 font-bold tracking-tight">
+              <h1 className="text-center xl:max-w-[600px] 2xl:max-w-none xl:text-left text-5xl md:text-7xl lg:text-8xl px-1 font-black tracking-tight">
                 I am{" "}
-                <strong className="text-primary dark:text-primary-light">
+                <span className="text-primary dark:text-primary-light">
                   Moonbami
-                </strong>
+                </span>
               </h1>
               <p className="text-center xl:text-left lg:text-lg xl:text-xl px-2 max-w-[500px] tracking-wide">
                 Pursuing a career for being a{" "}
@@ -34,11 +35,7 @@ const HomePage = async () => {
                 <EmailBtn href="mailto:Jam Moonbami <moonbamijamofficial15@gmail.com>" />
                 <GitHubBtn href="https://github.com/MoonbamiOfficial" />
               </div>
-              <Quote
-                quote="The greatest successes come from having the freedom to fail."
-                author="Mark Zuckerberg"
-                className=" opacity-80 xl:text-left"
-              />
+              <RandomizeQuote />
             </div>
             <div className="relative flex flex-col items-center gap-4">
               <div className="relative w-[250px] h-[250px] md:w-[300px] md:h-[300px] lg:w-[350px] lg:h-[350px] xl:w-[400px] xl:h-[400px] aspect-square rounded-xl shadow-md hover:shadow-primary-dark [&>span]:hover:opacity-70 overflow-hidden">
