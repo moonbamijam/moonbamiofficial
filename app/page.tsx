1c3b5d5ae106567de58f8b141754d4f23857e14e
Jam Moonbami <141120384+moonbamijam@users.noreply.github.com>
2024-09-10 14:19:15 +0800

    fix: hidden words in landing page (#47)
    # Type of Change
    
    <!-- Check what's necessary or check all if applicable -->
    
    - [ ] BREAKING CHANGE (change that would cause existing functionality to
    not work as expected)
    - [x] Feature (change that implements a new feature)
    - [x] Bug Fix (change that fixed a bug)
    - [ ] Improvements (change that improves existing features or
    performance)
    - [ ] Documentation (change in documents)
    - [ ] Release (new release)
    - [ ] Chore (formatting, refactoring, styling or other operations
    commit)
    
    ###
    
    # Description
    
    <!-- Please provide at least a short description of this pr. -->
    Fixed the hidden words in landing page due to the text have to in in bg
    clip text and transparent. Also move rgb animation to dark mode to avoid
    visually unappealing words in light mode
    
    ###
    
    # Related Tickets
    
    <!-- _If this pull request contains related or close an issue, please
    add them here. -->
    
    - Related Issue(s) #44 
    - Fixes #44
    
    ---------
    
    Co-authored-by: MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>

--- app/page.tsx
+++ app/page.tsx
@@ -14,17 +14,15 @@ const HomePage = async () => {
             <div className="flex flex-col items-center xl:items-start gap-6">
               <h1 className="text-center xl:max-w-[600px] 2xl:max-w-none xl:text-left text-5xl md:text-7xl lg:text-8xl px-1 font-black tracking-tight">
                 I am{" "}
-                <span className="animate-rgb bg-clip-text text-transparent">
-                  Moonbami
-                </span>
+                <span className="text-primary dark:animate-rgb">Moonbami</span>
               </h1>
               <p className="text-center xl:text-left lg:text-lg xl:text-xl px-2 max-w-[500px] tracking-wide">
                 Pursuing a career for being a{" "}
-                <strong className="animate-rgb bg-clip-text text-transparent">
+                <strong className="text-primary dark:animate-rgb">
                   developer
                 </strong>
                 . Still a novice, but making{" "}
-                <strong className="animate-rgb bg-clip-text text-transparent">
+                <strong className="text-primary dark:animate-rgb">
                   progress{" "}
                 </strong>
                 on a daily basis.
