4cd3be22078e24a855548d7524aaea83753ebd5e
Jam Moonbami <141120384+moonbamijam@users.noreply.github.com>
2024-09-24 22:31:07 +0800

    perf: enhancement to landing page (#52)
    # Type of Change
    
    <!-- Check what's necessary or check all if applicable -->
    
    - [ ] BREAKING CHANGE (change that would cause existing functionality to
    not work as expected)
    - [x] Feature (change that implements a new feature)
    - [ ] Bug Fix (change that fixed a bug)
    - [x] Improvements (change that improves existing features or
    performance)
    - [ ] Documentation (change in documents)
    - [ ] Release (new release)
    - [x] Chore (formatting, refactoring, styling or other operations
    commit)
    
    ###
    
    # Description
    
    <!-- Please provide at least a short description of this pr. -->
    Adjustments to landing page performance. Removed the dynamic landing
    page image due to performance issues
    
    ###
    
    # Related Tickets
    
    <!-- _If this pull request contains related or close an issue, please
    add them here. -->
    
    - Related Issue(s) #
    - Closes #
    
    ---------
    
    Co-authored-by: MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>

--- app/page.tsx
+++ app/page.tsx
@@ -4,48 +4,45 @@ import GitHubBtn from "@components/buttons/GitHubBtn";
 import Picture from "@components/common/Picture";
 import LandingSkeleton from "@components/skeletons/LandingSkeleton";
 import RandomizeQuote from "@components/RandomizeQuote";
-import { useFetch } from "@hooks/useFetch";
+import ImageCredits from "@components/ui/ImageCredits";
 
 const HomePage = async () => {
-  const { landingImage } = await useFetch("/api/landing-image");
-
   return (
     <Suspense fallback={<LandingSkeleton />}>
       <div className="container relative">
         <section id="home" className="h-screen">
           <div className="w-full h-full relative px-[5vw] flex flex-col justify-center items-center xl:flex-row xl:justify-around 2xl:justify-evenly gap-y-12">
-            <div className="flex flex-col items-center xl:items-start gap-6">
-              <h1 className="text-center xl:max-w-[600px] 2xl:max-w-none xl:text-left text-5xl md:text-7xl lg:text-8xl px-1 font-black tracking-tight">
-                I am{" "}
-                <span className="text-primary dark:animate-rgb">Moonbami</span>
-              </h1>
-              <p className="text-center xl:text-left lg:text-lg xl:text-xl px-2 max-w-[500px] tracking-wide">
-                Pursuing a career for being a{" "}
-                <strong className="text-primary dark:animate-rgb">
-                  developer
-                </strong>
-                . Still a novice, but making{" "}
-                <strong className="text-primary dark:animate-rgb">
-                  progress{" "}
-                </strong>
-                on a daily basis.
-              </p>
+            <div className="flex flex-col items-center xl:items-start gap-10">
+              <div className="flex flex-col items-center xl:items-start gap-5">
+                <h1 className="text-center xl:max-w-[600px] 2xl:max-w-none xl:text-left text-5xl md:text-7xl lg:text-8xl px-1 font-black tracking-tight">
+                  I am{" "}
+                  <span className="text-primary dark:animate-rgb">
+                    Moonbami
+                  </span>
+                </h1>
+                <p className="text-center xl:text-left text-lg lg:text-xl xl:text-2xl px-2 max-w-[600px] tracking-wide">
+                  Pursuing a career of being a{" "}
+                  <strong className="text-primary capitalize dark:animate-rgb">
+                    software engineer.
+                  </strong>
+                </p>
+              </div>
               <div className="flex gap-6">
                 <EmailBtn href="mailto:Jam Moonbami <moonbamijamofficial15@gmail.com>" />
                 <GitHubBtn href="https://github.com/moonbamijam" />
               </div>
-              <RandomizeQuote />
             </div>
             <div className="relative flex flex-col items-center gap-4">
               <div className="relative w-[250px] h-[250px] md:w-[300px] md:h-[300px] lg:w-[350px] lg:h-[350px] xl:w-[400px] xl:h-[400px] aspect-square rounded-xl shadow-md hover:shadow-primary-dark [&>span]:hover:opacity-70 overflow-hidden">
-                {landingImage?.map((image: any, id: number) => (
-                  <Picture
-                    key={id}
-                    src={image.image}
-                    alt={image.alt}
-                    isPriority={true}
-                  />
-                ))}
+                <Picture
+                  src={"mnbm_saktxa"}
+                  alt={"Jam Moonbami"}
+                  isPriority={true}
+                />
+                <ImageCredits
+                  creditsTo="Maki"
+                  creditLink="https://www.instagram.com/prettyboimaks/"
+                />
               </div>
             </div>
           </div>
