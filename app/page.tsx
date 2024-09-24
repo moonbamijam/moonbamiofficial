96ef50cde25c18a369cd42031a6ef27734aad4f8
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-09-24 22:21:51 +0800

    feat: adjustment to landing page texts

--- app/page.tsx
+++ app/page.tsx
@@ -12,27 +12,25 @@ const HomePage = async () => {
       <div className="container relative">
         <section id="home" className="h-screen">
           <div className="w-full h-full relative px-[5vw] flex flex-col justify-center items-center xl:flex-row xl:justify-around 2xl:justify-evenly gap-y-12">
-            <div className="flex flex-col items-center xl:items-start gap-6">
-              <h1 className="text-center xl:max-w-[600px] 2xl:max-w-none xl:text-left text-5xl md:text-7xl lg:text-8xl px-1 font-black tracking-tight">
-                I am{" "}
-                <span className="text-primary dark:animate-rgb">Moonbami</span>
-              </h1>
-              <p className="text-center xl:text-left lg:text-lg xl:text-xl px-2 max-w-[500px] tracking-wide">
-                Pursuing a career for being a{" "}
-                <strong className="text-primary dark:animate-rgb">
-                  developer
-                </strong>
-                . Still a novice, but making{" "}
-                <strong className="text-primary dark:animate-rgb">
-                  progress{" "}
-                </strong>
-                on a daily basis.
-              </p>
+            <div className="flex flex-col items-center xl:items-start gap-10">
+              <div className="flex flex-col items-center xl:items-start gap-5">
+                <h1 className="text-center xl:max-w-[600px] 2xl:max-w-none xl:text-left text-5xl md:text-7xl lg:text-8xl px-1 font-black tracking-tight">
+                  I am{" "}
+                  <span className="text-primary dark:animate-rgb">
+                    Moonbami
+                  </span>
+                </h1>
+                <p className="text-center xl:text-left text-lg lg:text-xl xl:text-2xl px-2 max-w-[600px] tracking-wide">
+                  Pursuing a career of being a{" "}
+                  <strong className="text-primary capitalize dark:animate-rgb">
+                    software engineer.
+                  </strong>
+                </p>
+              </div>
               <div className="flex gap-6">
                 <EmailBtn href="mailto:Jam Moonbami <moonbamijamofficial15@gmail.com>" />
                 <GitHubBtn href="https://github.com/MoonbamiOfficial" />
               </div>
-              <RandomizeQuote />
             </div>
             <div className="relative flex flex-col items-center gap-4">
               <div className="relative w-[250px] h-[250px] md:w-[300px] md:h-[300px] lg:w-[350px] lg:h-[350px] xl:w-[400px] xl:h-[400px] aspect-square rounded-xl shadow-md hover:shadow-primary-dark [&>span]:hover:opacity-70 overflow-hidden">
