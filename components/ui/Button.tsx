aa1989ff3a50ec8d7a07c269919af57ab30de145
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2025-02-23 21:41:26 +0800

    feat: add new button ui component

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -1,62 +1,48 @@
-import Link from "next/link";
-import React from "react";
+import { ButtonHTMLAttributes, forwardRef } from "react";
+import { VariantProps, cva } from "class-variance-authority";
+import cn from "@utils/cn";
 
-type ButtonProps = {
-  buttonType: string;
-  id: string;
-  text?: string;
-  icon?: React.ReactNode;
-  href?: string;
-  target?: string;
-  ariaLabel?: string;
-  className?: string;
-  refName?: React.LegacyRef<any>;
-  onClick?: React.MouseEventHandler<any>;
-  children?: React.ReactNode;
-};
+const buttonVariants = cva(
+  "flex justify-center items-center border-2 transform",
+  {
+    variants: {
+      variant: {
+        default:
+          "gap-2 rounded-lg border-primary bg-primary text-white hover:bg-primary-dark hover:border-primary-dark capitalize",
+        outline:
+          "gap-2 rounded-lg border-primary hover:bg-primary hover:text-white capitalize",
+        toggle:
+          "rounded-full border-transparent hover:border-primary hover:bg-primary text-xl active:scale-75",
+        icon: "rounded-full border-transparent text-xl",
+      },
+      size: {
+        default: "w-[150px] h-[50px]",
+        icon: "w-[40px] h-[40px]",
+        auto: "w-full h-full",
+        max: "w-max h-max",
+        min: "w-min h-min",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  },
+);
 
-export default function Button({
-  buttonType,
-  id,
-  text,
-  icon,
-  href,
-  target,
-  ariaLabel,
-  className,
-  refName,
-  onClick,
-  children,
-}: ButtonProps) {
-  if (buttonType.toLowerCase() === "button")
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> &
+  VariantProps<typeof buttonVariants> & {};
+
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, variant, size, ...props }, ref) => {
     return (
       <button
-        id={id}
-        ref={refName}
-        aria-label={ariaLabel}
-        onClick={onClick}
-        className={`transform ${className}`}
-      >
-        {icon}
-        <p>{text}</p>
-        {children}
-      </button>
+        ref={ref}
+        className={cn(buttonVariants({ variant, size, className }))}
+        {...props}
+      />
     );
-  else if (buttonType.toLowerCase() === "link")
-    return (
-      <>
-        {href && (
-          <Link
-            id={id}
-            href={href}
-            aria-label={ariaLabel}
-            target={target}
-            className={`w-[150px] h-[50px] rounded-lg border-2 gap-1 flex justify-center items-center capitalize transform ${className}`}
-          >
-            {icon}
-            {text}
-          </Link>
-        )}
-      </>
-    );
-}
+  },
+);
+
+export { Button, buttonVariants };
