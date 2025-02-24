15d5da0d3ef16bbb55a6588a3aaee3f0eddc82e1
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2025-02-24 20:42:08 +0800

    chore: update tailwind config content dir scope

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,10 +4,8 @@ import colors from "tailwindcss/colors";
 const config: Config = {
   darkMode: "class",
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./layouts/**/*.{js,ts,jsx,tsx,mdx}",
+    "./frontend/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     colors: {
