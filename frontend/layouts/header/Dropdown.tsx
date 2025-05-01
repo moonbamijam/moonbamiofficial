564159a376ac76e4527547c5510d21e5d5d3724c
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2025-05-01 23:26:17 +0800

    refactor/feat: move dropdown to header layout and update its ui

--- frontend/layouts/header/Dropdown.tsx
+++ frontend/layouts/header/Dropdown.tsx
@@ -7,7 +7,7 @@ export default function Dropdown({ children, position }: Props) {
   return (
     <nav
       id="main"
-      className={`absolute ${position} text-base flex flex-col gap-2 mt-2 mr-2 px-2 py-8 rounded-xl bg-surface dark:bg-dm-surface border border-transparent`}
+      className={`absolute ${position} text-base flex flex-col gap-2 mt-2 mr-2 px-4 py-8 rounded-lg bg-surface dark:bg-dm-surface shadow z-[2000]`}
     >
       {children}
     </nav>
