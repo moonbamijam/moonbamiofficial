444cd4af932eb5f6a94ec121ef76ffdfe777affe
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2025-05-01 23:30:21 +0800

    refactor: remove more page

--- frontend/ts/constants/navLinks.ts
+++ frontend/ts/constants/navLinks.ts
@@ -3,5 +3,4 @@ export const NavLinks = [
   { id: 2, name: "about", path: "/about" },
   { id: 3, name: "projects", path: "/projects" },
   { id: 4, name: "timeline", path: "/timeline" },
-  { id: 5, name: "more", path: "/more" },
 ];
