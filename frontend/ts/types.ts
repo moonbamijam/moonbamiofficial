66d2c9459322f9e94e1c55b24ea145e7df464f11
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2025-07-21 11:35:20 +0800

    feat: add spotify ts files

--- frontend/ts/types.ts
+++ frontend/ts/types.ts
@@ -1,4 +1,30 @@
+import { Dispatch, SetStateAction } from "react";
+
 export type spotifyEmbedsType = {
   src: string;
   title: string;
+};
+
+export type spotifyNowPlayingType = {
+  albumImageUrl: string;
+  artist: string;
+  isPlaying: boolean;
+  songUrl: string;
+  title: string;
+  timePlayed?: number;
+  timeTotal?: number;
+  artistUrl?: string;
+};
+
+export type spotifyNowPlayingContextType = {
+  spotify: {
+    spotifyNowPlaying: spotifyNowPlayingType;
+    setSpotifyNowPlaying: Dispatch<SetStateAction<spotifyNowPlayingType>>;
+  };
+};
+
+export type spotifyAccessTokensType = {
+  CLIENT_ID: string | undefined;
+  CLIENT_SECRET: string | undefined;
+  REFRESH_TOKEN: string | undefined;
 };
