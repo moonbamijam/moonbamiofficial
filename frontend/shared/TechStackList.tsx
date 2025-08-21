a0bcb6b4f783483fb434f94c7a2f6e7358bc2020
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2025-08-21 18:42:05 +0800

    seperate tech stack list from tech stack slider component

---
+++ frontend/shared/TechStackList.tsx
@@ -0,0 +1,89 @@
+import {
+  SiTypescript,
+  SiJavascript,
+  SiTailwindcss,
+  SiExpress,
+  SiMongodb,
+  SiPnpm,
+  SiPython,
+  SiReact,
+  SiSass,
+  SiVite,
+  SiAdobephotoshop,
+  SiKrita,
+} from "react-icons/si";
+import { RiNextjsFill } from "react-icons/ri";
+import { FaCss3Alt, FaNodeJs } from "react-icons/fa";
+import { FaHtml5 } from "react-icons/fa6";
+import { BiLogoVisualStudio } from "react-icons/bi";
+
+export const techStackList = [
+  {
+    icon: <SiTypescript />,
+    position: 1,
+  },
+  {
+    icon: <SiJavascript />,
+    position: 2,
+  },
+  {
+    icon: <SiTailwindcss />,
+    position: 3,
+  },
+  {
+    icon: <SiExpress />,
+    position: 4,
+  },
+  {
+    icon: <FaCss3Alt />,
+    position: 5,
+  },
+  {
+    icon: <FaHtml5 />,
+    position: 6,
+  },
+  {
+    icon: <SiMongodb />,
+    position: 7,
+  },
+  {
+    icon: <SiPnpm />,
+    position: 8,
+  },
+  {
+    icon: <SiPython />,
+    position: 9,
+  },
+  {
+    icon: <SiReact />,
+    position: 10,
+  },
+  {
+    icon: <SiSass />,
+    position: 11,
+  },
+  {
+    icon: <SiVite />,
+    position: 12,
+  },
+  {
+    icon: <RiNextjsFill />,
+    position: 13,
+  },
+  {
+    icon: <FaNodeJs />,
+    position: 14,
+  },
+  {
+    icon: <BiLogoVisualStudio />,
+    position: 15,
+  },
+  {
+    icon: <SiAdobephotoshop />,
+    position: 16,
+  },
+  {
+    icon: <SiKrita />,
+    position: 17,
+  },
+];
