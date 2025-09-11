3e089b4346648fb0964ef27d28974a209845aeff
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2025-09-12 00:05:23 +0800

    variant styling adjustments to button ui component

--- frontend/components/ui/Button.tsx
+++ frontend/components/ui/Button.tsx
@@ -6,11 +6,11 @@ const buttonVariants = cva("flex justify-center items-center transform", {
   variants: {
     variant: {
       default:
-        "gap-2 rounded-lg border-primary bg-primary text-white hover:bg-primaryvariant capitalize font-semibold",
+        "gap-2 rounded-lg border-primary bg-primary text-white dark:text-black hover:bg-primaryvariant capitalize font-semibold",
       outline:
         "gap-2 rounded-lg border-2 border-primary hover:bg-primary hover:text-white capitalize font-semibold",
       ghost:
-        "gap-2 rounded-lg bg-[#EBEBEB] dark:bg-dm-surface-light hover:bg-primary dark:hover:bg-primary dark:hover:text-black capitalize font-semibold",
+        "gap-2 rounded-lg bg-slate-300 dark:bg-slate-600 hover:bg-primary dark:hover:bg-primary dark:text-white capitalize font-semibold",
       toggle: "rounded-full border-transparent hover:bg-primary text-xl",
       icon: "rounded-full text-xl",
       nav: "capitalize rounded-lg font-semibold px-4 xl:px-6 py-2",
