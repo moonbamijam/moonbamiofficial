132ba53edfb11422e10008dc2d70fe87ba431bb4
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2025-09-12 00:04:21 +0800

    add new variant and styling adjustments to image ui component

--- frontend/components/ui/Image.tsx
+++ frontend/components/ui/Image.tsx
@@ -5,12 +5,14 @@ import cn from "@frontend/ts/utils/cn";
 const imageVariants = cva("relative w-full h-full rounded-lg overflow-hidden", {
   variants: {
     variant: {
-      default: "shadow mb-4",
+      default: "shadow mb-2",
       bakground: "absolute -z-10",
+      thumbnail: "rounded-xl",
     },
     size: {
       default: "min-w-[300px] min-h-[300px] max-h-[500px]",
       full: "w-full h-full",
+      thumbnail: "min-h-[200px] max-h-[600px]",
     },
     shape: {
       square: "aspect-square",
