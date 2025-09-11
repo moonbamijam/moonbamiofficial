f168c4dcd0835bca586e2b3f6d1cc91ae9257064
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2025-09-12 00:03:11 +0800

    new variant and base styling adjustments to paragraph ui component

--- frontend/components/ui/Paragraph.tsx
+++ frontend/components/ui/Paragraph.tsx
@@ -3,12 +3,13 @@ import { VariantProps, cva } from "class-variance-authority";
 import cn from "@frontend/ts/utils/cn";
 
 const paragraphVariants = cva(
-  "px-2 w-full min-w-[400px] text-base sm:text-lg xl:text-xl text-slate-500 dark:text-slate-400",
+  "w-full min-w-[300px] text-base sm:text-lg xl:text-xl text-slate-500 dark:text-slate-400",
   {
     variants: {
       variant: {
         default: "max-w-[600px]",
         wide: "max-w-[800px]",
+        brief: "max-w-[500px] line-clamp-2",
       },
     },
     defaultVariants: {
