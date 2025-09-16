4bff53f1900c576bdc4e772a382662f7827d5f1e
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2025-09-16 17:52:33 +0800

    new projects page skeleton screen

--- app/projects/loading.tsx
+++ app/projects/loading.tsx
@@ -1,5 +1,10 @@
-import ProjectSkeleton from "@frontend/layouts/skeletons/ProjectSkeleton";
+import PageWrapper from "@frontend/layouts/common/PageWrapper";
+import SkeletonScreenProject from "@frontend/layouts/skeleton-screens/SkeletonScreenProject";
 
 export default function Loading() {
-  return <ProjectSkeleton />;
+  return (
+    <PageWrapper>
+      <SkeletonScreenProject />
+    </PageWrapper>
+  );
 }
