36d20549e55909c664c4ef0831042899b634663d
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2025-09-18 23:32:24 +0800

    adjust tech stack sizes in homepage

--- frontend/layouts/TechStackSlider.tsx
+++ frontend/layouts/TechStackSlider.tsx
@@ -5,7 +5,7 @@ import "@frontend/styles/tech-stack-slider.css";
 export default function TechStackSlider() {
   return (
     <div className="slider w-full h-max text-slate-400 dark:text-slate-500 overflow-hidden">
-      <p className="text-center mb-2">Skill Arsenal</p>
+      <p className="text-center mb-2 text-sm sm:text-base">Tech Stack</p>
       <div className="items relative w-full flex h-[8vw] lg:h-[5vw] items-center gap-8">
         {techStackList.map((techStack, id) => (
           <div
@@ -16,7 +16,7 @@ export default function TechStackSlider() {
                 "--items": 17,
               } as CSSProperties
             }
-            className="item animate-slideTectStack absolute left-[100%] text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl w-max flex justify-center hover:text-primary"
+            className="item animate-slideTectStack absolute left-[100%] text-4xl sm:text-5xl xl:text-6xl 2xl:text-7xl w-max flex justify-center hover:text-primary"
           >
             {techStack.icon}
           </div>
