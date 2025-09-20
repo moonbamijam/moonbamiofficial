4acd6d300e9eae88e18681a3e74f5276ed5763a5
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2025-09-20 21:47:56 +0800

    add id to props in section common layout

--- frontend/layouts/common/Section.tsx
+++ frontend/layouts/common/Section.tsx
@@ -1,12 +1,14 @@
 type SectionProps = {
   children: React.ReactNode;
   className?: string;
+  id?: string;
 };
 
-export default function Section({ children, className }: SectionProps) {
+export default function Section({ children, className, id }: SectionProps) {
   return (
     <section
-      className={`w-full h-full relative gap-4 lg:gap-6 py-[50px] lg:py-[100px]  ${className}`}
+      id={id}
+      className={`w-full h-full relative gap-4 lg:gap-6 py-[50px] lg:py-[100px] ${className}`}
     >
       {children}
     </section>
