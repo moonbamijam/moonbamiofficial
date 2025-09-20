1d8af6c6e8dff5b0d6f9a3e0df55edf93e70acf5
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2025-09-20 20:51:40 +0800

    few styling adjustments to image ui component

--- frontend/components/ui/Image.tsx
+++ frontend/components/ui/Image.tsx
@@ -5,12 +5,12 @@ import cn from "@frontend/ts/utils/cn";
 const imageVariants = cva("relative w-full h-full rounded-lg overflow-hidden", {
   variants: {
     variant: {
-      default: "shadow mb-2",
+      default: "shadow",
       bakground: "absolute -z-10",
       thumbnail: "rounded-xl",
     },
     size: {
-      default: "min-w-[300px] min-h-[300px] max-h-[500px]",
+      default: "min-w-[300px] min-h-[300px] xl:min-h-[400px]",
       full: "w-full h-full",
       thumbnail: "min-h-[200px] max-h-[600px]",
     },
