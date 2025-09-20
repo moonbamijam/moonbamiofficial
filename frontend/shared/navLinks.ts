4db5dfde1f67205ab7f07fbe10683983c68625f7
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2025-09-20 20:52:51 +0800

    add contact simple contact page

--- frontend/shared/navLinks.ts
+++ frontend/shared/navLinks.ts
@@ -3,4 +3,5 @@ export const NavLinks = [
   { id: 2, name: "about", path: "/about" },
   { id: 3, name: "projects", path: "/projects" },
   { id: 4, name: "timeline", path: "/timeline" },
+  { id: 5, name: "contact", path: "/contact" },
 ];
