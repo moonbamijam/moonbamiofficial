fbca24779c6eee7443f431059747d1f0c029df4e
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2025-09-21 15:21:59 +0800

    small content adjustment to personal section in about page

--- frontend/layouts/pages/about/sections/personal/AboutPersonal.tsx
+++ frontend/layouts/pages/about/sections/personal/AboutPersonal.tsx
@@ -49,7 +49,7 @@ export default function AboutPersonal() {
           </span>{" "}
           consists of two of my most favorite things in my life. Moon represents
           how I love <span className="text-primary font-semibold">space</span>{" "}
-          in which I talk about{" "}
+          in which I talked about{" "}
           <Link
             href="#space"
             className="text-primary font-semibold hover:underline"
