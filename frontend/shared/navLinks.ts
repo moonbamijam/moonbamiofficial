0eb55023eb6e39679eeba5fca3f67ddf87fc3063
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2026-01-03 18:14:17 +0800

    add experience route to navlinks

--- frontend/shared/navLinks.ts
+++ frontend/shared/navLinks.ts
@@ -1,7 +1,8 @@
 export const NavLinks = [
-  { id: 1, name: "home", path: "/" },
-  { id: 2, name: "about", path: "/about" },
-  { id: 3, name: "projects", path: "/projects" },
-  { id: 4, name: "timeline", path: "/timeline" },
-  { id: 5, name: "contact", path: "/contact" },
+  { name: "home", path: "/" },
+  { name: "about", path: "/about" },
+  { name: "experience", path: "/experience" },
+  { name: "projects", path: "/projects" },
+  { name: "timeline", path: "/timeline" },
+  { name: "contact", path: "/contact" },
 ];
