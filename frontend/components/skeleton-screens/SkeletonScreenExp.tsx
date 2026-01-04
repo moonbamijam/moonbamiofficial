2b51e4630ce7e4cb425c022b57f7104f6a61da48
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2026-01-04 19:55:14 +0800

    add skeleton screen for experience page

---
+++ frontend/components/skeleton-screens/SkeletonScreenExp.tsx
@@ -0,0 +1,33 @@
+import Section from "@frontend/layouts/common/Section";
+import SkeletonScreen from "@frontend/components/skeleton-screens/SkeletonScreen";
+import { Image } from "@frontend/components/ui/Image";
+
+export default function SkeletonScreenExp() {
+  return (
+    <>
+      <Section className="grid grid-cols-1 xl:grid-cols-4 premium:grid-cols-5 z-10">
+        <SkeletonScreen className="w-full xl:col-span-2 premium:col-span-3 flex flex-col items-end order-2">
+          <Image className="opacity-0 invisible" shape="tv" />
+        </SkeletonScreen>
+        <div className="xl:col-span-2 h-max">
+          <SkeletonScreen className="min-h-12" />
+          <SkeletonScreen className="mt-4 min-h-52" />
+        </div>
+      </Section>
+      <Section className="!p-0">
+        <SkeletonScreen>
+          <Image className="opacity-0 invisible max-h-[600px]" shape="tv" />
+        </SkeletonScreen>
+      </Section>
+      <Section className="grid grid-cols-1 xl:grid-cols-4 premium:grid-cols-5 z-10">
+        <SkeletonScreen className="w-full xl:col-span-2 premium:col-span-3 order-2 flex flex-col items-end">
+          <Image className="opacity-0 invisible" shape="tv" />
+        </SkeletonScreen>
+        <div className="xl:col-span-2 h-max">
+          <SkeletonScreen className="min-h-12" />
+          <SkeletonScreen className="mt-4 min-h-52" />
+        </div>
+      </Section>
+    </>
+  );
+}
