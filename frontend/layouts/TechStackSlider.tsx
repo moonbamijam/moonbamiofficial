e0e43a190f49e12390b9fb662e0ef41e2c608769
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2026-01-17 17:21:52 +0800

    duplicate the items for infinite repeat

--- frontend/layouts/TechStackSlider.tsx
+++ frontend/layouts/TechStackSlider.tsx
@@ -1,26 +1,30 @@
-import { CSSProperties } from "@node_modules/@types/react";
 import { techStackList } from "@frontend/shared/TechStackList";
-import "@frontend/styles/tech-stack-slider.css";
 
 export default function TechStackSlider() {
   return (
-    <div className="slider w-full h-max text-slate-400 dark:text-slate-500 overflow-hidden">
+    <div className="w-full h-max overflow-hidden">
       <p className="text-center mb-2 text-sm sm:text-base">Tech Stack</p>
-      <div className="items relative w-full flex h-[8vw] lg:h-[5vw] items-center gap-8">
-        {techStackList.map((techStack, id) => (
-          <div
-            key={id}
-            style={
-              {
-                "--position": techStack.position,
-                "--items": 17,
-              } as CSSProperties
-            }
-            className="item animate-slideTectStack absolute left-[100%] text-4xl sm:text-5xl xl:text-6xl 2xl:text-7xl w-max flex justify-center hover:text-primary"
-          >
-            {techStack.icon}
-          </div>
-        ))}
+      <div className="slider flex text-slate-400 dark:text-slate-500">
+        <div className="flex h-[8vw] lg:h-[5vw] justify-center items-center gap-12 pr-12 animate-slideTechStack">
+          {techStackList.map((techStack, id) => (
+            <div
+              key={id}
+              className="text-4xl sm:text-5xl xl:text-6xl 2xl:text-7xl hover:text-primary"
+            >
+              {techStack.icon}
+            </div>
+          ))}
+        </div>
+        <div className="flex h-[8vw] lg:h-[5vw] justify-center items-center gap-12 pr-12 animate-slideTechStack">
+          {techStackList.map((techStack, id) => (
+            <div
+              key={id}
+              className="text-4xl sm:text-5xl xl:text-6xl 2xl:text-7xl hover:text-primary"
+            >
+              {techStack.icon}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
