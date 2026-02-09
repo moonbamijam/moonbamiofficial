a21ee03ee614c251c18b64ad2734934b2c2146e7
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2026-02-09 14:54:15 +0800

    add transition to timeline page components

--- src/components/EventWrapper.tsx
+++ src/components/EventWrapper.tsx
@@ -12,20 +12,20 @@ export default function EventWrapper({
   return (
     <div
       key={_id}
-      className="w-full lg:w-max flex py-8 opacity-80 dark:opacity-70 hover:opacity-100 group duration-500"
+      className="w-full lg:w-max flex py-8 opacity-80 dark:opacity-70 hover:opacity-100 group transition-opacity"
     >
       <Button
         variant="icon"
         size="max"
-        className="text-4xl bg-white dark:bg-dm-surface group-hover:bg-primary p-4 rounded-lg shadow-lg [&>svg>path]:group-hover:text-white [&>svg>path]:dark:group-hover:text-black"
+        className="text-4xl bg-surface group-hover:bg-primary p-4 rounded-lg shadow-lg [&>svg>path]:group-hover:text-white [&>svg>path]:dark:group-hover:text-black transition-colors"
       >
         {typeIcon}
       </Button>
       <div className="px-4">
-        <h1 className="group-hover:text-primary text-4xl font-black capitalize">
+        <h1 className="group-hover:text-primary text-4xl font-black capitalize transition-colors">
           {date}
         </h1>
-        <h3 className="text-3xl font-bold text-slate-500 group-hover:text-slate-700 dark:group-hover:text-slate-400">
+        <h3 className="text-3xl font-bold text-slate-500 group-hover:text-slate-700 dark:group-hover:text-slate-400 transition-colors">
           {note}
         </h3>
         <Paragraph className="mt-1">{message}</Paragraph>
