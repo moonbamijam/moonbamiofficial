912e582d1b26ec78deb15aed80c59c99ba1726cc
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2026-02-10 08:55:37 +0800

    add not found image to next config remote patterns

--- next.config.ts
+++ next.config.ts
@@ -17,6 +17,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   images: {
     remotePatterns: allRemotePatternsURL([
+      "https://i.pinimg.com/originals/9d/26/92/9d2692b51ebf08955da879128c57ab69.gif",
       "https://avatars.githubusercontent.com/u/141120384?v=4",
       "https://images.unsplash.com/photo-1515879218367-8466d910aaa4?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cHJvZ3JhbW1pbmd8ZW58MHx8MHx8fDA%3D",
       "https://sarahjmwalker.wordpress.com/wp-content/uploads/2014/12/interstellar-2014-poster-wallpaper.jpg",
