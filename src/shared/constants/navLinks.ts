626ce527d2f5d4f9cf2123d9c5cb87185ad78287
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2026-02-11 18:49:49 +0800

    remove old and refactored files

--- src/shared/constants/navLinks.ts
+++
@@ -1,8 +0,0 @@
-export const NavLinks = [
-  { name: "home", path: "/" },
-  { name: "about", path: "/about" },
-  { name: "experience", path: "/experience" },
-  { name: "projects", path: "/projects" },
-  { name: "timeline", path: "/timeline" },
-  { name: "contact", path: "/contact" },
-];
