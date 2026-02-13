932a2b3a2f09a5b95f2a746b5a4b5dfb8a46fb9b
Jam Moonbami <141120384+moonbamijam@users.noreply.github.com>
2026-02-13 08:20:17 +0800

    feat: move resume locally (#112)
    # Type of Change
    
    <!-- Check what's necessary or check all if applicable -->
    
    - [ ] BREAKING CHANGE (change that would cause existing functionality to
    not work as expected)
    - [x] Feature (change that implements a new feature)
    - [ ] Bug Fix (change that fixed a bug)
    - [ ] Improvements (change that improves existing features or
    performance)
    - [ ] Documentation (change in documents)
    - [ ] Release (new release)
    - [ ] Chore (formatting, refactoring, styling or other operations
    commit)
    
    ###
    
    # Description
    
    <!-- Please provide at least a short description of this pr. -->
    
    ###
    
    # Related Tickets
    
    <!-- _If this pull request contains related or close an issue, please
    add them here. -->
    
    - Related Issue(s) #
    - Closes #
    
    Co-authored-by: MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>

--- src/components/buttons/ResumeButton.tsx
+++ src/components/buttons/ResumeButton.tsx
@@ -5,7 +5,7 @@ import { buttonVariants } from "../ui/variants";
 export default function ResumeButton() {
   return (
     <Link
-      href="https://drive.google.com/file/d/1CZlNfJm_M-_OGxNFEteoEfe-g-Q19FoJ/view"
+      href="/resume-v2.pdf"
       download
       target="_blank"
       className={`${buttonVariants({ variant: "ghost" })}`}
