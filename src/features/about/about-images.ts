b6e5b27b570d9ef89afd2f8494d2c9492082cb2e
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2026-02-14 21:54:06 +0800

    add list of images for about page personal section

---
+++ src/features/about/about-images.ts
@@ -0,0 +1,115 @@
+import { carouselType } from "./about.types";
+
+export const personalCarousel: carouselType[] = [
+  {
+    src: "/images/pages/about/about.jpg",
+    alt: "jam moonbami",
+    width: 1500,
+    height: 1500,
+
+    credits: {
+      href: "https://www.instagram.com/lakezeus_/",
+      name: "Chris De Real",
+      year: 2023,
+    },
+  },
+  {
+    src: "/images/pages/about/gang.jpg",
+    alt: "jam moonbami and the boys",
+    width: 1500,
+    height: 1500,
+    credits: {
+      href: "https://www.instagram.com/_ateliermaki/",
+      name: "Maki",
+      year: 2024,
+    },
+  },
+  {
+    src: "/images/pages/about/jam.JPG",
+    alt: "jam moonbami",
+    width: 1500,
+    height: 1500,
+    credits: {
+      href: "https://www.instagram.com/_ateliermaki/",
+      name: "Maki",
+      year: 2024,
+    },
+  },
+  {
+    src: "/images/pages/about/rizzy.JPG",
+    alt: "jam moonbami",
+    width: 1500,
+    height: 1500,
+    credits: {
+      href: "https://www.instagram.com/_ateliermaki/",
+      name: "Maki",
+      year: 2024,
+    },
+  },
+  {
+    src: "/images/pages/about/stolen.JPG",
+    alt: "jam moonbami",
+    width: 1500,
+    height: 1500,
+    credits: {
+      href: "https://www.instagram.com/_ateliermaki/",
+      name: "Maki",
+      year: 2024,
+    },
+  },
+  {
+    src: "/images/pages/about/capy.jpg",
+    alt: "jam moonbami",
+    width: 1500,
+    height: 1500,
+    credits: {
+      href: "",
+      name: "Eibba",
+      year: 2025,
+    },
+  },
+  {
+    src: "/images/pages/about/you.jpg",
+    alt: "jam moonbami",
+    width: 1500,
+    height: 1500,
+    credits: {
+      href: "",
+      name: "Leen",
+      year: 2025,
+    },
+  },
+  {
+    src: "/images/pages/about/wm.jpg",
+    alt: "jam moonbami",
+    width: 1500,
+    height: 1500,
+    credits: {
+      href: "",
+      name: "Leen",
+      year: 2025,
+    },
+  },
+  {
+    src: "/images/pages/about/gradv1.JPG",
+    alt: "jam moonbami",
+    width: 1500,
+    height: 1500,
+    credits: {
+      href: "https://www.facebook.com/napsceaenterprises/",
+      name: "NAPS CEA Enterprises",
+      year: 2025,
+    },
+  },
+  {
+    src: "/images/pages/about/gradv2.JPG",
+    alt: "jam moonbami",
+    width: 1500,
+    height: 1500,
+    credits: {
+      href: "https://www.facebook.com/napsceaenterprises/",
+      name: "NAPS CEA Enterprises",
+      year: 2025,
+    },
+  },
+];
