0c3e30a7ffe8e3aba602d2992fa6b62cc5ff3c5d
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2026-02-16 18:51:21 +0800

    adjust spacing in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ const HomePage = async () => {
     <Suspense fallback={<SkeletonScreenHome />}>
       <section id="home" className="container pt-32 pb-10">
         {/* wraps the introduction and the tech stacks */}
-        <div className="w-full h-full relative flex flex-col justify-around lg:justify-between items-center gap-20">
+        <div className="w-full h-full relative flex flex-col justify-around lg:justify-between items-center gap-25">
           {/* wraps the intro */}
           <div className="w-full flex flex-col justify-evenly items-center xl:flex-row gap-5 gap-y-16">
             <div className="relative w-full flex flex-col items-center xl:items-start gap-5 max-w-150">
